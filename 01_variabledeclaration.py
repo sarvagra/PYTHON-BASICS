c8d9775ffb013cccff32ec61d6103839a411d5a2
sarvagra <sarvagras977@gmail.com>
2024-04-27 21:08:10 +0530

    solved issues

---
+++ 01_variabledeclaration.py
@@ -0,0 +1,12 @@
+# declaring variables in python 
+a=5          #int type
+b=4.4        #float type
+c="SARVAGRA" #string type
+d='q'        #char type
+x= True      #boolean type
+
+ 
+list=['I','am' ,'learning', 'python']     #list 
+mtuple=('this','is','tuple')              #tuple
+set1={'pen','pencil','eraser'}            #set
+dict={"name":"sarvagra","gender":"male"}  #dictionary
